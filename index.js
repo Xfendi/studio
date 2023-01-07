14b1423321a3da0e756652e36d13ece28feee09f
xFendi11 <115659159+Xfendi@users.noreply.github.com>
2023-01-07 20:24:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const name = 'xFendi Studio'
+const color = '#FFE000'
+const color1 = '#00C1FF'
+
+const footer_copy = document.getElementById('footer_copy');
+const luk_had  = document.getElementById('luk_had');
+const typed_had = document.getElementById('typed_had');
+
+typed_had.style = `color: ${color1};`
+
+luk_had.style = `color: ${color};`
+
+function clint_soon() {
+    alert('soon')
+}
+
+console.log(name)
